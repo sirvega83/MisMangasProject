10dd12e62624161e2d282d109be04a593216b4bf
Said Vega <sirvega83@gmail.com>
2023-12-15 18:32:27 +0100

    Initial Commit

---
+++ MisMangas/MisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasApp.swift
+//  MisMangas
+//
+//  Created by Said Vega Ibn Rouhou on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
